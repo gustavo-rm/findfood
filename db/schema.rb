ec472134685e3424e78f3bf1f89b61f5e464400c
Gustavo <gustavomalacarne@alunos.utfpr.edu.br>
2019-04-03 19:44:53 -0300

    Aula 03/04/2019

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_03_222825) do
+ActiveRecord::Schema.define(version: 2019_04_03_223756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2019_04_03_222825) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
+    t.integer "author_id"
   end
 
 end
