136dd07fbe2801465f8e12fb1aa107686b30374e
Gustavo <gustavomalacarne@alunos.utfpr.edu.br>
2019-04-10 19:26:48 -0300

    Aula 10/04

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	def menu(description, path)
+		content_tag(:li) do
+			link_to description, path
+		end
+	end
+
+	def resume(text)
+		truncate(text, length: 300)
+	end
 end
